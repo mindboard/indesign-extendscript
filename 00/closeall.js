4dd11d12a4eb5f6881272e23c9c6aec4c7a2837b
osima <blue@osima.jp>
2015-10-23 17:01:50 +0900

    added 00 and 01 examples.

---
+++ 00/closeall.js
@@ -0,0 +1,6 @@
+while( true ){
+	if( app.documents.length<1 ){ break; }
+
+	var doc  = app.activeDocument;
+	doc.close(SaveOptions.no);
+}
---
+++ 01/hello-world.js
@@ -0,0 +1,30 @@
+#target InDesign-11 
+
+var params = {};
+params.documentPreferences = {
+	pageWidth:"210mm",
+	pageHeight:"297mm",
+	facingPages:false};
+
+// add document
+var doc = app.documents.add(params);
+
+// reset page margin
+var page = doc.pages.item(0);
+page.marginPreferences.properties = {
+	top:"10mm",
+	left:"10mm",
+	bottom:"10mm",
+	right:"10mm"};
+
+// add textFrame
+var textFrame = page.textFrames.add({
+	geometricBounds:["20mm","20mm","40mm","110mm"], // top,left,bottom,right
+	contents:'Hello World!'});
+
+// change paragraph style
+for( var i=0; i<textFrame.paragraphs.length; i++ ){
+	var paragraph = textFrame.paragraphs[i];
+	paragraph.pointSize = 36;
+   	paragraph.appliedParagraphStyle.appliedFont = app.fonts.item('Kozuka Gothic Pro');
+}
