145e3c2d120b498f8c6ec869ebc92467de4c7d7f
osima <blue@osima.jp>
2015-10-26 10:10:29 +0900

    added simple version hello-world.js.

---
+++ 01/simple-hello-world.js
@@ -0,0 +1,12 @@
+
+// add document
+var doc = app.documents.add();
+
+// reset page margin
+var page = doc.pages.item(0);
+
+// add textFrame
+var textFrame = page.textFrames.add();
+textFrame.geometricBounds = ["20mm","20mm","40mm","110mm"];
+textFrame.contents = 'Hello World!';
+
