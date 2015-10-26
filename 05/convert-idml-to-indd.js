4af7f10ade2ca544c86c7947ee313d6e2667afba
osima <blue@osima.jp>
2015-10-26 12:11:49 +0900

    added idml to indd example.

---
+++ 05/convert-idml-to-indd.js
@@ -0,0 +1,28 @@
+
+app.scriptPreferences.userInteractionLevel = UserInteractionLevels.NEVER_INTERACT;
+
+var openDocument = function( idmlFile ){ return app.open( idmlFile ); };
+var closeDocument = function( document ){ document.close(SaveOptions.no); }
+
+var saveDocumentAsIndd = function( inddFile, document ){ document.save( inddFile ); }
+var saveDocumentAsIdml = function( idmlFile, document ){ doc.exportFile(ExportFormat.INDESIGN_MARKUP, idmlFile, false); };
+
+var toInddFile = function( idmlFile ){
+	var len = idmlFile.name.length;
+	var suffixLen = ".idml".length;
+	return File(idmlFile.path+"/"+idmlFile.name.substr( 0,len-suffixLen )+".indd");
+}
+
+var currentDir = function(){
+	return File($.fileName).parent;
+};
+
+
+var dir = Folder( currentDir().fullName + "/res/" );
+var files = dir.getFiles('*.idml');
+for(var i=0; i<files.length; i++){
+	var idmlFile = files[i];
+	var doc = openDocument( idmlFile );
+	saveDocumentAsIndd( toInddFile(idmlFile), doc ); 
+	closeDocument( doc );
+}
