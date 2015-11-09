73ab5cee5ee8882df954821198a3a1381b380108
osima <blue@osima.jp>
2015-11-09 17:46:48 +0900

    added bezier example.

---
+++ 08/beizer.js
@@ -0,0 +1,35 @@
+var params = {};
+params.documentPreferences = {
+	pageWidth   : 100,
+	pageHeight  : 100,
+	facingPages : false};
+
+// add document
+var doc = app.documents.add(params);
+
+// reset page margin
+var page = doc.pages.item(0);
+page.marginPreferences.properties = {top:0, left:0, bottom:0, right:0};
+
+//
+// add polygon
+//
+var polygon = page.polygons.add({});
+polygon.strokeWeight = '0.25mm';
+
+var pointA = [20,100];
+var pointB = [30,60];
+var pointC = [40,20];
+
+var pointD = [60,20];
+var pointE = [70,60];
+var pointF = [80,100];
+
+var entirePath = [];
+entirePath.push( [pointA, pointB, pointC] );
+entirePath.push( [pointD, pointE, pointF] );
+
+polygon.paths[0].properties = {
+	entirePath: entirePath,
+   	pathType:	PathType.OPEN_PATH
+};
