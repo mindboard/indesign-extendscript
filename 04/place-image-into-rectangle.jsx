efbd1567cc8d8f741be48fec37bb44b862f4c790
Tomoaki Oshima <tomo@osima.jp>
2016-10-18 10:12:37 +0900

    added a place-image example.

---
+++ 04/place-image-into-rectangle.jsx
@@ -0,0 +1,54 @@
+
+var createDocument = function(params){
+    params.documentPreferences = {
+        pageWidth   : params.pageWidth+"mm",
+        pageHeight  : params.pageHeight+"mm",
+           facingPages : false};
+
+    var doc = app.documents.add(params);
+
+    var page = doc.pages.item(0);
+    page.marginPreferences.properties = {
+        top    : params.marginTop+"mm",
+        left   : params.marginLeft+"mm",
+        bottom : params.marginBottom+"mm",
+        right  : params.marginRight+"mm"};
+
+    return doc;
+};
+
+var createRectangle = function(page,params){
+    return page.rectangles.add({
+        geometricBounds:[
+            params.top+"mm",
+            params.left+"mm",
+            params.bottom+"mm",
+            params.right+"mm"] });
+};
+
+var currentDir = function(){
+    return File($.fileName).parent;
+};
+
+
+var pageParams = {
+    pageWidth   : 100,
+    pageHeight  : 100,
+    marginTop   : 10,
+    marginLeft  : 10,
+    marginBottom: 10,
+    marginRight : 10};
+
+var doc = createDocument( pageParams );
+
+var page = doc.pages.item(0);
+
+var rectangleParams = {
+    top    : pageParams.marginTop,
+    left   : pageParams.marginLeft,
+    bottom : (pageParams.pageHeight - pageParams.marginBottom),
+    right  : (pageParams.pageWidth  - pageParams.marginRight) };
+
+var rectangle = createRectangle(page, rectangleParams);
+rectangle.place( File( currentDir().fullName + '/links/tiger.eps' ) );
+rectangle.fit(FitOptions.CONTENT_TO_FRAME);
