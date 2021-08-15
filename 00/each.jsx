18eb6f28377c312256f311c69d45895c9fdc5537
Tomoaki Oshima <tomo.osima@gmail.com>
2021-08-15 10:43:41 +0900

    Add an index value callback feature.

--- 00/each.jsx
+++ 00/each.jsx
@@ -2,7 +2,7 @@
 
 var eachItem = function(myItemList,func){
     for(var i=0; i<myItemList.length; i++){
-        func(myItemList[i]);
+        func(myItemList[i], i);
     }
 };
 
@@ -14,5 +14,5 @@ for( var i=0; i<list.length; i++ ){
 }
 
 $.writeln('--- test2 ---');
-var myFunc = function(it){ $.writeln( it ); };
+var myFunc = function(item, index){ $.writeln( '- ('+index+') '+item ); };
 eachItem( list, myFunc );
